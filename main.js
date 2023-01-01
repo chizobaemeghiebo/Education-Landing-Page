cfa240020f738da787e4774ef7146b3ebbaa501f
Emeghiebo Chizoba <77364360+chizoba-009@users.noreply.github.com>
2023-01-01 21:53:31 +0100

    Made overall ui changes

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 const open = document.querySelector('.open')
 const close = document.querySelector('.close')
 const nav = document.querySelector('.ul')
+
+const main = document.querySelector('body')
+main.addEventListener('click', (e) => {
+  if (e.target !== open && e.target !== close) {
+    console.log('wtf')
+    nav.style.opacity = '0'
+    close.style.display = 'none'
+    open.style.display = 'block'
+  }
+})
+
 open.addEventListener('click', () => {
   open.style.display = 'none'
   close.style.display = 'block'
+  nav.style.width = '50%'
   nav.style.opacity = '1'
 })
-close.addEventListener('click', () => {
-  close.style.display = 'none'
+
+close.addEventListener('click', (e) => {
+  e.target.style.display = 'none'
   open.style.display = 'block'
+  nav.style.width = '1%'
   nav.style.opacity = '0'
 })
